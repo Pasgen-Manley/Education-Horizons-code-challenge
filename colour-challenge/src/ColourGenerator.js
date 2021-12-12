88790d427799a71f249a0e00d4441264fbe438ee
Pasgen-Manley <pasgen95@gmail.com>
2021-12-12 17:44:35 +0800

    Added code comments

--- colour-challenge/src/ColourGenerator.js
+++ colour-challenge/src/ColourGenerator.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ColourGenerator = () => {
   
   const rgbColours = createColours();
-
+  //Function to break down colour components into 32 steps. 8 x 32 = 256
   function createSteps(start, end) {
     let steps = [];
     for (let i = start; i <= end; i++) {
@@ -19,7 +19,8 @@ const ColourGenerator = () => {
     let green = createSteps(8, 256);
     let blue = createSteps(8, 256);
     let colourArray = [];
-
+    //Using a while loop that runs through and takes a random integer from red, green and blue and splices them from there.
+    // That integer is then used to created the rbg values of a colour
     let i = 0;
     while (i < red.length) {
       const redIndex = Math.floor(Math.random() * red.length);
@@ -40,14 +41,16 @@ const ColourGenerator = () => {
           const greenIndex = Math.floor(Math.random() * green.length);
           const greenValue = green[greenIndex];
           green.splice(greenIndex, 0);
-
+          //random rgb values inputting into a literal to be used to display a rgb colour in CSS
           const rgb = `rgb(${redValue},${blueValue},${greenValue})`;
+          //rbg values are then pushed into the colour array
           colourArray.push(rgb);
 
           k++
         }
       }
     }
+    //Colours array is then shuffled and randomised thanks to the Knuth shuffle algorithm
     return shuffleFunction(colourArray);
   };
   //Fisher-Yates (aka Knuth) Shuffle.
@@ -68,7 +71,7 @@ const ColourGenerator = () => {
   
     return array;
   }
-
+  //JSX styling
   return (
     rgbColours.map((colour, index) => {
       return (
